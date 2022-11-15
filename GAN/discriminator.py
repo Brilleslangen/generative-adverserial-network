ca5cec5bc894b5d129aafc552a31eb4c6a071ea3
Erlend Rønning <erleronn@stud.ntnu.no>
2022-11-15 11:21:45 +0100

    More refactoring. Removed print statement

--- GAN/discriminator.py
+++ GAN/discriminator.py
@@ -3,44 +3,44 @@ from traitlets.config.application import OrderedDict
 
 
 class Discriminator(nn.Module):
-    def __init__(self, fm_size, channels, num_layers):
+    def __init__(self, fm_size, channels, num_channels):
         super().__init__()
-        layers = OrderedDict()
+        modules = OrderedDict()
 
         # Layer 1: Convolution with leaky ReLU
-        layers['L1-conv'] = nn.Conv2d(
+        modules['L1-conv'] = nn.Conv2d(
             in_channels=channels, 
             out_channels=fm_size, 
             kernel_size=4,
             stride=2, 
             padding=1, 
             bias=False)
-        layers['L1-norm'] = nn.BatchNorm2d(fm_size)
-        layers['L1-leaky-relu'] = nn.LeakyReLU(negative_slope=0.2, inplace=True)
+        modules['L1-norm'] = nn.BatchNorm2d(fm_size)
+        modules['L1-leaky-relu'] = nn.LeakyReLU(negative_slope=0.2, inplace=True)
         
-        for i in range(2, num_layers+2):
-            layers[f'L{i}-conv'] = nn.Conv2d(
+        for i in range(2, num_channels+2):
+            modules[f'L{i}-conv'] = nn.Conv2d(
                 in_channels=fm_size * 2 ** (i-2),
                 out_channels=fm_size * 2 ** (i-1),
                 kernel_size=4,
                 stride=2,
                 padding=1
             )
-            layers[f'L{i}-norm'] = nn.BatchNorm2d(fm_size * 2 ** (i-1))
-            layers[f'L{i}-leaky-relu'] = nn.LeakyReLU(negative_slope=0.2, inplace=True)
+            modules[f'L{i}-norm'] = nn.BatchNorm2d(fm_size * 2 ** (i-1))
+            modules[f'L{i}-leaky-relu'] = nn.LeakyReLU(negative_slope=0.2, inplace=True)
 
         # Final convolution layer with sigmoid
-        layers[f'L{num_layers+2}-conv'] = nn.Conv2d(
-            in_channels=fm_size * 2 ** (num_layers),
+        modules[f'L{num_channels+2}-conv'] = nn.Conv2d(
+            in_channels=fm_size * 2 ** (num_channels),
             out_channels=1,
             kernel_size=4,
             stride=1,
             padding=0,
             bias=False
         )
-        layers[f'L{num_layers+2}-sigmoid'] = nn.Sigmoid()
+        modules[f'L{num_channels+2}-sigmoid'] = nn.Sigmoid()
 
-        self.model = nn.Sequential(layers)
+        self.model = nn.Sequential(modules)
 
         print(self.model)
     def forward(self, input):
